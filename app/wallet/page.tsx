3fcb15eaba4b6e434c59a05c74e94a0b98d49cd1
kamrulkoche <kamrulkoche@gmail.com>
2024-07-29 02:25:20 +0600

    update  wallet page

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -8,13 +8,13 @@ const Layout = dynamic(() => import('@/app/layout/dashboardlayout'), { ssr: fals
 const page = () => {
     return (
         <Layout>
-            <div className='pb-12'>
+            <div className=''>
                 <h2 className='text-sm font-medium mb-4' >Wallet Balance</h2>
                 <div className='grid md:grid-cols-2 gap-6 '>
                     <Balance />
                     <CreateWallet />
                 </div>
-                <div className='flex justify-between items-center mt-8'>
+                <div className='flex justify-between items-center mt-8 pb-12 '>
                     <h2 className='text-black text-sm font-medium'>Wallet Log</h2>
                     <button className='text-white text-base font-bold py-2 px-5 bg-[#723EEB] hover:bg-[#5e21ee] duration-500 rounded-3xl '>View More</button>
                 </div>
